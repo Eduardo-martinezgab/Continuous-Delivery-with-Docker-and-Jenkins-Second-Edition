5ec096eb8da1c7fb73e2d8d43ba7ba9af6ddd8a5
Eduardo-martinezgab <emartinezgab@gmail.com>
2026-06-22 20:33:51 -0400

    Code changes week04

---
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorControllerTest.java
@@ -0,0 +1,37 @@
+package com.leszko.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class) 
+@WebMvcTest(CalculatorController.class)
+public class CalculatorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Calculator calculator;
+
+    @Test
+    public void testSumEndpoint() throws Exception {
+
+        when(calculator.sum(anyInt(), anyInt())).thenReturn(5);
+
+        mockMvc.perform(get("/sum")
+                .param("a", "2")
+                .param("b", "3"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("5"));
+    }
+}
